c5450a793665fc61f1b6da41b3b442345bf6f5ad
plutasnyy <kamil.plucinski97@gmail.com>
2017-07-10 18:50:08 +0200

    created model for notes

--- note/models.py
+++ note/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Note(models.Model):
+    title=models.CharField(max_length=64)
+    created_on=models.DateTimeField(auto_now_add=True)
+    content=models.CharField(max_length=5000)
+
+    def __str__(self):
+        return self.name
