3b35c4aab40b499f2408e015e27ee22bcc49fa85
plutasnyy <kamil.plucinski97@gmail.com>
2017-07-10 19:02:22 +0200

    created hyperlinks and new foreignkey in note

--- note/models.py
+++ note/models.py
@@ -7,7 +7,8 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-    author = models.CharField(User.get_username())
+
+    author = models.ForeignKey('auth.User')
 
     def __str__(self):
         return self.name
