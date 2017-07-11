1b05f1b67655546a10ebbeb95bda90f10803d65a
plutasnyy <kamil.plucinski97@gmail.com>
2017-07-11 22:57:07 +0200

    view work fine, modyfided key to looking for note by id not username

--- note/models.py
+++ note/models.py
@@ -7,7 +7,8 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-    author = models.ForeignKey('auth.User')
+    author = models.TextField('auth.User')
+    id = models.AutoField(primary_key=True)
 
     def __str__(self):
         return self.title
--- note/views.py
+++ note/views.py
@@ -4,8 +4,8 @@ from note.models import Note as Note_model
 
 def Homepage(request):
     if request.user.is_authenticated():
-        username = request.user.username
-        notes_list=Note_model.objects.all()
+        username = request.user.id
+        notes_list=Note_model.objects.all().filter(author=username)
         return render(request,'note.html',{'notes_list':notes_list})
     else:
         return HttpResponseRedirect('/')
