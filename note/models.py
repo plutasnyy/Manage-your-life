6f96fd36480ae95d5b74f8103b9c414968d25537
plutasnyy <kamil.plucinski97@gmail.com>
2017-07-13 23:13:57 +0200

    ekhm :/

--- note/models.py
+++ note/models.py
@@ -7,7 +7,7 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
-    author = models.TextField('auth.User')
+    author = models.ForeignKey(User.id)
 
     def __str__(self):
         return self.title
