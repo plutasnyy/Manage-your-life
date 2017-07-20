e1dc7ff5b6f73d55e22f39914f84070b57aeef14
plutasnyy <kamil.plucinski97@gmail.com>
2017-07-20 13:30:43 +0200

    created relationship and models

--- note/models.py
+++ note/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from datetime import datetime
 
-from django.conf import settings
 from django.contrib.auth.models import User
 
 class Note(models.Model):
--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+class Item(models.Model):
+    id=models.AutoField(primary_key=True)
+    list=models.ForeignKey(List)
+    content=models.TextField())
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class List(models.Model):
+    id=models.AutoField(primary_key=True)
+    created_by=ForeignKey(User)
+    title=models.TextField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
