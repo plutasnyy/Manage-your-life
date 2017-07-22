e1ed90c1e3de8aa6e90e7c21eb7df27df401eb10
plutasnyy <kamil.plucinski97@gmail.com>
2017-07-22 16:17:41 +0200

    some work with todolist

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView,ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from todo_list.models import List, Item
 
-def Homepage(request):
-    return render(request,'todo_list.html',{})
+class TodoList(ListView):
+    model = List
+    queryset=List.objects.all()
+    template_name='todo_list.html'
+
+class TodoEdit(UpdateView):
+    pass
+
+class TodoDelete(DeleteView):
+    pass
+
+class TodoCreate(CreateView):
+    pass
