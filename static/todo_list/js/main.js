9775fb903b4ed70b7075555d6bb5f9e7aae10b25
plutasnyy <kamil.plucinski97@gmail.com>
2017-07-26 17:29:41 +0200

    more cleaning in skeleton

--- static/todo_list/js/main.js
+++ static/todo_list/js/main.js
@@ -6,6 +6,7 @@ $(function () {
       dataType: 'json',
       beforeSend: function () {
         $("#modal-add_list").modal("show");
+        alert("lol");
       },
       success: function (data) {
         $("#modal-book .modal-content").html(data.html_form);
