af8a1c9b23e86b6adfecb3508ed4c379f00d50e3
plutasnyy <kamil.plucinski97@gmail.com>
2017-07-27 12:34:33 +0200

    modyfided js to me

--- static/todo_list/js/main.js
+++ static/todo_list/js/main.js
@@ -13,3 +13,23 @@ $(function () {
     });
   });
 });
+
+$("#modal-book").on("submit", ".js-list-create-form", function () {
+  var form = $(this);
+  $.ajax({
+    url: form.attr("action"),
+    data: form.serialize(),
+    type: form.attr("method"),
+    dataType: 'json',
+    success: function (data) {
+      if (data.form_is_valid) {
+        //$("#book-table tbody").html(data.html_book_list);  // <-- Replace the table body
+        $("#modal-book").modal("hide");  // <-- Close the modal
+      }
+      else {
+        $("#modal-add_list .modal-content").html(data.html_form);
+      }
+    }
+  });
+  return false;
+});
