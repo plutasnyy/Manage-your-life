278d15820ff9d93ae3761a3b8114f75379609f54
plutasnyy <kamil.plucinski97@gmail.com>
2017-10-06 12:20:32 +0200

    added error messages in models

--- calendar_app/models.py
+++ calendar_app/models.py
@@ -6,11 +6,11 @@ class Event(models.Model):
     title = models.CharField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User)
-    start_date = models.DateField()
-    end_date = models.DateField()
+    start_date = models.DateField(error_messages={'required': 'Please enter your start date'})
+    end_date = models.DateField(error_messages={'required': 'Please enter your end date'})
 
-    start_time = models.TimeField()
-    end_time = models.TimeField()
+    start_time = models.TimeField(error_messages={'required': 'Please enter your start time'})
+    end_time = models.TimeField(error_messages={'required': 'Please enter your end time'})
 
     all_day_event = models.BooleanField()
 
